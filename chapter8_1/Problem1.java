ba0a20b414130e76f851d547030bcee57157d4f9
ueno08022 <132907969+ueno08022@users.noreply.github.com>
2023-05-08 21:51:37 +0900

    初回コミット
    コミット確認

---
+++ chapter8_1/Problem1.java
@@ -0,0 +1,42 @@
+package chapter8_1;
+
+public class Problem1 {
+	
+	String name;
+	int hp;
+	public void sleep() {
+		this.hp = 100;
+		System.out.println(this.name+"は、眠っていた");
+		
+	}
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
